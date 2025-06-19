f7101fe2ac3907c06b364d206cc6d872dc9e22de
Konsta Purtsi <konsta@arado.fi>
2025-06-19 19:01:20 +0300

    chore: improve test for liquidate when user has multiple loans

--- contracts/loan_manager/src/contract.rs
+++ contracts/loan_manager/src/contract.rs
@@ -1157,29 +1157,35 @@ mod tests {
             pool_xlm_addr,
             pool_usdc_client,
             pool_usdc_addr,
+            pool_eurc_client,
+            eurc_asset_client,
             xlm_asset_client,
             usdc_asset_client,
+            pool_eurc_addr,
             ..
         } = setup_test_env(&e);
 
         // print more money
         usdc_asset_client.mint(&admin, &9_001);
+        eurc_asset_client.mint(&admin, &9_001);
+        xlm_asset_client.mint(&user, &30_000);
         pool_usdc_client.deposit(&admin, &9_001);
-        xlm_asset_client.mint(&user, &12_000);
+        pool_eurc_client.deposit(&admin, &9_001);
 
         // ACT
         // Create a loan.
         manager_client.create_loan(&user, &10_000, &pool_usdc_addr, &12_505, &pool_xlm_addr);
+        manager_client.create_loan(&user, &10_000, &pool_eurc_addr, &12_505, &pool_xlm_addr);
 
-        let user_loan = manager_client.get_loan(&user, &0);
-
-        assert_eq!(user_loan.borrowed_amount, 10_000);
+        let loans = manager_client.get_loans(&user);
+        let usdc_loan = loans.get(0).unwrap();
+        assert_eq!(usdc_loan.borrowed_amount, 10_000);
 
         manager_client.add_interest(&user);
 
         // Here borrowed amount should be the same as time has not moved. add_interest() is only called to store the LastUpdate sequence number.
-        assert_eq!(user_loan.borrowed_amount, 10_000);
-        assert_eq!(user_loan.health_factor, 10_004_000);
+        assert_eq!(usdc_loan.borrowed_amount, 10_000);
+        assert_eq!(usdc_loan.health_factor, 10_004_000);
 
         // Move time
         e.ledger().with_mut(|li| {
@@ -1193,11 +1199,11 @@ mod tests {
 
         manager_client.add_interest(&user);
 
-        let user_loan = manager_client.get_loan(&user, &0);
+        let usdc_loan = manager_client.get_loan(&user, &0);
 
-        assert_eq!(user_loan.borrowed_amount, 12_998);
-        assert_eq!(user_loan.health_factor, 7_696_568);
-        assert_eq!(user_loan.collateral_amount, 12_505);
+        assert_eq!(usdc_loan.borrowed_amount, 12_998);
+        assert_eq!(usdc_loan.health_factor, 7_696_568);
+        assert_eq!(usdc_loan.collateral_amount, 12_505);
 
         e.ledger().with_mut(|li| {
             li.sequence_number = 100_000 + 1_000;
@@ -1208,11 +1214,18 @@ mod tests {
 
         manager_client.liquidate(&admin, &user, &0, &5_000);
 
-        let user_loan = manager_client.get_loan(&user, &0);
+        let loans = manager_client.get_loans(&user);
+        assert_eq!(loans.len(), 2);
 
-        assert_eq!(user_loan.borrowed_amount, 7_998);
-        assert_eq!(user_loan.health_factor, 7_256_814);
-        assert_eq!(user_loan.collateral_amount, 7_255);
+        let usdc_loan = loans.get(0).unwrap();
+        assert_eq!(usdc_loan.borrowed_amount, 7_998);
+        assert_eq!(usdc_loan.health_factor, 7_256_814);
+        assert_eq!(usdc_loan.collateral_amount, 7_255);
+
+        let eurc_loan = loans.get(1).unwrap();
+        assert_eq!(eurc_loan.borrowed_amount, 12_998);
+        assert_eq!(eurc_loan.health_factor, 7_696_568);
+        assert_eq!(eurc_loan.collateral_amount, 12_505);
     }
 
     /* Test setup helpers */
