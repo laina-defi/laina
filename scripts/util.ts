f12aaa8054f27e24b6e82e1f681d33840d8478cb
Teolhyn <teemu.hynna@gmail.com>
2025-08-18 18:29:23 +0300

    chore: update dependencies to stables for p23

--- scripts/util.ts
+++ scripts/util.ts
@@ -1,122 +1,115 @@
-import { execSync } from "child_process";
-import { mkdirSync, readFileSync, writeFileSync } from "fs";
-import path from "path";
+import { execSync } from 'child_process';
+import { mkdirSync, readFileSync, writeFileSync } from 'fs';
+import path from 'path';
 
 // Load environment variables starting with PUBLIC_ into the environment,
 // so we don't need to specify duplicate variables in .env
 for (const key in process.env) {
-	if (key.startsWith("PUBLIC_")) {
-		process.env[key.substring(7)] = process.env[key];
-	}
+  if (key.startsWith('PUBLIC_')) {
+    process.env[key.substring(7)] = process.env[key];
+  }
 }
 
 // The stellar-sdk Client requires (for now) a defined public key. These are
 // the Genesis accounts for each of the "typical" networks, and should work as
 // a valid, funded network account.
 const GENESIS_ACCOUNTS = {
-	public: "GAAZI4TCR3TY5OJHCTJC2A4QSY6CJWJH5IAJTGKIN2ER7LBNVKOCCWN7",
-	testnet: "GBRPYHIL2CI3FNQ4BXLFMNDLFJUNPU2HY3ZMFSHONUCEOASW7QC7OX2H",
-	futurenet: "GADNDFP7HM3KFVHOQBBJDBGRONMKQVUYKXI6OYNDMS2ZIK7L6HA3F2RF",
-	standalone: "GBZXN7PIRZGNMHGA7MUUUF4GWPY5AYPV6LY4UV2GL6VJGIQRXFDNMADI",
+  public: 'GAAZI4TCR3TY5OJHCTJC2A4QSY6CJWJH5IAJTGKIN2ER7LBNVKOCCWN7',
+  testnet: 'GBRPYHIL2CI3FNQ4BXLFMNDLFJUNPU2HY3ZMFSHONUCEOASW7QC7OX2H',
+  futurenet: 'GADNDFP7HM3KFVHOQBBJDBGRONMKQVUYKXI6OYNDMS2ZIK7L6HA3F2RF',
+  standalone: 'GBZXN7PIRZGNMHGA7MUUUF4GWPY5AYPV6LY4UV2GL6VJGIQRXFDNMADI',
 };
 
 // @ts-ignore
 const network = process.env.SOROBAN_NETWORK;
-export const GENESIS_ACCOUNT =
-	GENESIS_ACCOUNTS[network as keyof typeof GENESIS_ACCOUNTS] ??
-	GENESIS_ACCOUNTS.testnet;
+export const GENESIS_ACCOUNT = GENESIS_ACCOUNTS[network as keyof typeof GENESIS_ACCOUNTS] ?? GENESIS_ACCOUNTS.testnet;
 
 export const loadAccount = () => {
-	// This takes the secret key from SOROBAN_SECRET_KEY env-variable, so make sure you have that set.
-	exe(`stellar keys add ${process.env.SOROBAN_ACCOUNT}`);
+  // This takes the secret key from SOROBAN_SECRET_KEY env-variable, so make sure you have that set.
+  exe(`stellar keys add ${process.env.SOROBAN_ACCOUNT}`);
 };
 
 // Function to execute and log shell commands
 export const exe = (command: string) => {
-	console.log(command);
-	return execSync(command, { stdio: "inherit" });
+  console.log(command);
+  return execSync(command, { stdio: 'inherit' });
 };
 
 export const buildContracts = () => {
-	exe(`rm -f ./target/wasm32v1-none/release/*.wasm`);
-	exe(`rm -f ./target/wasm32v1-none/release/*.d`);
-	exe(`make build`);
+  exe(`rm -f ./target/wasm32v1-none/release/*.wasm`);
+  exe(`rm -f ./target/wasm32v1-none/release/*.d`);
+  exe(`make build`);
 };
 
 /** Install all contracts and save their wasm hashes to .stellar */
 export const installContracts = () => {
-	const contractsDir = `./.stellar/contract-wasm-hash`;
-	mkdirSync(contractsDir, { recursive: true });
+  const contractsDir = `./.stellar/contract-wasm-hash`;
+  mkdirSync(contractsDir, { recursive: true });
 
-	install("loan_manager");
-	install("loan_pool");
+  install('loan_manager');
+  install('loan_pool');
 };
 
 /* Install a contract */
 const install = (contractName: string) => {
-	exe(
-		`stellar contract upload \
+  exe(
+    `stellar contract upload \
 --wasm ./target/wasm32v1-none/release/${contractName}.wasm \
 --ignore-checks \
 > ./.stellar/contract-wasm-hash/${contractName}.txt`,
-	);
+  );
 };
 
 export const filenameNoExtension = (filename: string) => {
-	return path.basename(filename, path.extname(filename));
+  return path.basename(filename, path.extname(filename));
 };
 
-export const readTextFile = (path: string): string =>
-	readFileSync(path, { encoding: "utf8" }).trim();
+export const readTextFile = (path: string): string => readFileSync(path, { encoding: 'utf8' }).trim();
 
 // This is a function so its value can update during init.
 export const loanManagerAddress = (): string =>
-	process.env.CONTRACT_ID_LOAN_MANAGER ||
-	readTextFile("./.stellar/contract-ids/loan_manager.txt");
+  process.env.CONTRACT_ID_LOAN_MANAGER || readTextFile('./.stellar/contract-ids/loan_manager.txt');
 
 export const createContractBindings = () => {
-	bind("loan_manager", process.env.CONTRACT_ID_LOAN_MANAGER);
-	bind("pool_xlm", process.env.CONTRACT_ID_POOL_XLM);
-	bind("pool_usdc", process.env.CONTRACT_ID_POOL_USDC);
-	bind("pool_eurc", process.env.CONTRACT_ID_POOL_EURC);
+  bind('loan_manager', process.env.CONTRACT_ID_LOAN_MANAGER);
+  bind('pool_xlm', process.env.CONTRACT_ID_POOL_XLM);
+  bind('pool_usdc', process.env.CONTRACT_ID_POOL_USDC);
+  bind('pool_eurc', process.env.CONTRACT_ID_POOL_EURC);
 };
 
 const bind = (contractName: string, address: string | undefined) => {
-	const address_ =
-		address || readTextFile(`./.stellar/contract-ids/${contractName}.txt`);
-	exe(
-		`stellar contract bindings typescript --contract-id ${address_} --output-dir ./packages/${contractName} --overwrite`,
-	);
-	exe(
-		`cd ./packages/${contractName} && npm install && npm run build && cd ../..`,
-	);
+  const address_ = address || readTextFile(`./.stellar/contract-ids/${contractName}.txt`);
+  exe(
+    `stellar contract bindings typescript --contract-id ${address_} --output-dir ./packages/${contractName} --overwrite`,
+  );
+  exe(`cd ./packages/${contractName} && npm install && npm run build && cd ../..`);
 };
 
 export const createContractImports = () => {
-	const CONTRACTS = ["loan_manager", "pool_xlm", "pool_usdc", "pool_eurc"];
-	CONTRACTS.forEach(importContract);
+  const CONTRACTS = ['loan_manager', 'pool_xlm', 'pool_usdc', 'pool_eurc'];
+  CONTRACTS.forEach(importContract);
 };
 
 const importContract = (contractName: string) => {
-	const outputDir = `./src/contracts/`;
-	mkdirSync(outputDir, { recursive: true });
+  const outputDir = `./src/contracts/`;
+  mkdirSync(outputDir, { recursive: true });
 
-	/* eslint-disable quotes */
-	/* eslint-disable no-constant-condition */
-	const importContent =
-		`import * as Client from '${contractName}'; \n` +
-		`import { rpcUrl } from './util'; \n\n` +
-		`export const contractId = Client.networks.${process.env.SOROBAN_NETWORK}.contractId; \n\n` +
-		`export const contractClient = new Client.Client({ \n` +
-		`  ...Client.networks.${process.env.SOROBAN_NETWORK}, \n` +
-		`  rpcUrl, \n` +
-		`${process.env.SOROBAN_NETWORK === "local" || "standalone" ? `  allowHttp: true,\n` : null}` +
-		`  publicKey: '${GENESIS_ACCOUNT}', \n` +
-		`}); \n`;
+  /* eslint-disable quotes */
+  /* eslint-disable no-constant-condition */
+  const importContent =
+    `import * as Client from '${contractName}'; \n` +
+    `import { rpcUrl } from './util'; \n\n` +
+    `export const contractId = Client.networks.${process.env.SOROBAN_NETWORK}.contractId; \n\n` +
+    `export const contractClient = new Client.Client({ \n` +
+    `  ...Client.networks.${process.env.SOROBAN_NETWORK}, \n` +
+    `  rpcUrl, \n` +
+    `${process.env.SOROBAN_NETWORK === 'local' || 'standalone' ? `  allowHttp: true,\n` : null}` +
+    `  publicKey: '${GENESIS_ACCOUNT}', \n` +
+    `}); \n`;
 
-	const outputPath = `${outputDir}/${contractName}.ts`;
-	writeFileSync(outputPath, importContent);
-	console.log(`Created import for ${contractName}`);
+  const outputPath = `${outputDir}/${contractName}.ts`;
+  writeFileSync(outputPath, importContent);
+  console.log(`Created import for ${contractName}`);
 };
 
 export const logDeploymentInfo = () => {
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,53 +1,53 @@
-import { Button } from "@components/Button";
-import Identicon from "@components/Identicon";
-import { useWallet } from "@contexts/wallet-context";
-import type { PropsWithChildren } from "react";
-import { Link } from "react-router-dom";
+import { Button } from '@components/Button';
+import Identicon from '@components/Identicon';
+import { useWallet } from '@contexts/wallet-context';
+import type { PropsWithChildren } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
-	const { wallet, openConnectWalletModal, disconnectWallet } = useWallet();
+  const { wallet, openConnectWalletModal, disconnectWallet } = useWallet();
 
-	return (
-		<nav className="relative mx-auto flex justify-between items-center pt-12 pb-6 px-4 max-w-full w-[74rem]">
-			<div>
-				<Link to="/">
-					<img src="laina_v3_shrinked.png" alt="logo" className="w-32" />
-				</Link>
-			</div>
+  return (
+    <nav className="relative mx-auto flex justify-between items-center pt-12 pb-6 px-4 max-w-full w-[74rem]">
+      <div>
+        <Link to="/">
+          <img src="laina_v3_shrinked.png" alt="logo" className="w-32" />
+        </Link>
+      </div>
 
-			<div className="hidden md:flex flex-row ml-auto mr-8">
-				<LinkItem to="/">Laina</LinkItem>
-				<LinkItem to="/lend">App</LinkItem>
-			</div>
+      <div className="hidden md:flex flex-row ml-auto mr-8">
+        <LinkItem to="/">Laina</LinkItem>
+        <LinkItem to="/lend">App</LinkItem>
+      </div>
 
-			{!wallet ? (
-				<Button onClick={openConnectWalletModal}>Connect wallet</Button>
-			) : (
-				<div className="dropdown dropdown-end">
-					<button tabIndex={0} type="button">
-						<Identicon address={wallet.address} />
-					</button>
-					<ul className="dropdown-content rounded-box bg-white mt-1 mr-1 w-64 z-[1] p-4 shadow">
-						<li className="px-8 py-4">
-							<p className="font-semibold">{wallet.displayName}</p>
-							<p className="text-grey leading-tight">{wallet.name}</p>
-						</li>
-						<li>
-							<Button variant="outline" onClick={disconnectWallet}>
-								Disconnect Wallet
-							</Button>
-						</li>
-					</ul>
-				</div>
-			)}
-		</nav>
-	);
+      {!wallet ? (
+        <Button onClick={openConnectWalletModal}>Connect wallet</Button>
+      ) : (
+        <div className="dropdown dropdown-end">
+          <button tabIndex={0} type="button">
+            <Identicon address={wallet.address} />
+          </button>
+          <ul className="dropdown-content rounded-box bg-white mt-1 mr-1 w-64 z-[1] p-4 shadow">
+            <li className="px-8 py-4">
+              <p className="font-semibold">{wallet.displayName}</p>
+              <p className="text-grey leading-tight">{wallet.name}</p>
+            </li>
+            <li>
+              <Button variant="outline" onClick={disconnectWallet}>
+                Disconnect Wallet
+              </Button>
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
 }
 
 const LinkItem = ({ to, children }: PropsWithChildren<{ to: string }>) => {
-	return (
-		<Link to={to} className="text-base font-semibold p-4 hover:underline">
-			{children}
-		</Link>
-	);
+  return (
+    <Link to={to} className="text-base font-semibold p-4 hover:underline">
+      {children}
+    </Link>
+  );
 };
